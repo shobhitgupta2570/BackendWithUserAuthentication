85872643c1bb2545e01c7801f3fa091a391a65c7
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-01-11 19:21:30 +0530

    User with authentication

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+//routes import
+import userRouter from './routes/user.routes.js'
+import adminRouter from './routes/admin.routes .js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/admin", adminRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+export { app }
